2172cac206042220347661fa93859d3dfe43eb82
yaz_1710 <yahyahosny1234567@gmail.com>
2026-09-08 10:34:19 +0300

    1st answer session3

--- assignment22/Program.cs
+++ assignment22/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿string title = "Refactoring";
+switch (title)
+{
+    case "Clean Code":
+        Console.WriteLine("Great choice!");
+        break;
+    case "Refactoring":
+        Console.WriteLine("Nice pick!");
+        break;
+    default:
+        Console.WriteLine("Never heard of it");
+        break;
+} //1st answer session3
