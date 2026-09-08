e55dea8d35dffbb549128fe8da1864a0aebdb475
yaz_1710 <yahyahosny1234567@gmail.com>
2026-09-08 10:36:39 +0300

    2nd answer session3

--- assignment22/Program.cs
+++ assignment22/Program.cs
@@ -1,13 +1,3 @@
-﻿string title = "Refactoring";
-switch (title)
-{
-    case "Clean Code":
-        Console.WriteLine("Great choice!");
-        break;
-    case "Refactoring":
-        Console.WriteLine("Nice pick!");
-        break;
-    default:
-        Console.WriteLine("Never heard of it");
-        break;
-} //1st answer session3
+﻿int pages = 464;
+string sizeLabel = pages > 300 ? "Long Book" : "Short Book";
+//2nd answer session3
